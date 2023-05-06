b7308e0d62fdfd1611b46246a75e4465ad8f35dc
harshdigi <harshkahuja@gmail.com>
2023-05-06 19:56:05 +0530

    Addded Registered User Api

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import HackathonCreateAPIView, register_hackathon, HackathonSubmissionAPIView
+from .views import HackathonCreateAPIView, register_hackathon, HackathonSubmissionAPIView,HackathonUser
 
 router = routers.SimpleRouter()
 router.register('hackathons', HackathonCreateAPIView, basename="hackathon")
 router.register('submission', HackathonSubmissionAPIView, basename="submission")
+router.register('registered_user', HackathonUser, basename="registered_users" )
 urlpatterns = [
     path('', include(router.urls)),
     path('register_hackathon',view= register_hackathon, name= "register_hackathon")
